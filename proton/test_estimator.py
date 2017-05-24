f23d66a433997cc8494f65656438dd565c1a0acd
Emmanouil Stergiadis <em.stergiadis@gmail.com>
2017-05-24 14:49:09 +0200

    added some ToDos

--- proton/test_estimator.py
+++ proton/test_estimator.py
@@ -6,6 +6,8 @@ import random
 from proton.estimator import AccuracyHandler, LinearBEDPredictor, BEDPredictorUpperBoundNaive
 from proton.optimizer import LPOptimizer
 
+# ToDo:
+# Plotting code should either go to the data exploration notebook or into a dedicated file/module
 if __name__ == "__main__":
     # Read data
     data = pd.read_csv('../data/PayoffMatrix.txt', delim_whitespace=True, header=None)
@@ -14,7 +16,7 @@ if __name__ == "__main__":
     ah = AccuracyHandler(benefit, range(2, 7))
 
     optimizer = LPOptimizer()
-    #ah.draw_evaluation_plot(optimizer)
+    ah.draw_evaluation_plot(optimizer)
     lower_bound = ah.get_predictor_solution(LinearBEDPredictor(benefit), optimizer)
     upper_bound = ah.get_predictor_solution(BEDPredictorUpperBoundNaive(benefit), optimizer)
 
